72d1a9c779ac0c5dba4999534be9634140d64bc5
Jose Gabriel Tortolero <josegabrieltortolero@gmail.com>
2021-05-23 19:51:26 -0300

    resume, contact, more css

--- app/forms.py
+++ app/forms.py
@@ -5,8 +5,8 @@ from wtforms.validators import DataRequired, Email, Length
 
 
 class ContactForm(FlaskForm):
-    name = StringField("Name:", validators=[DataRequired()])
-    subject = StringField("Subject:", validators=[DataRequired()])
-    email = StringField("Email:", validators=[DataRequired(), Email(message="Enter a valid Email"), Length(max=120)])
-    message = TextAreaField("Message:", validators=[DataRequired()])
+    name = StringField("Name*", validators=[DataRequired()])
+    subject = StringField("Subject*", validators=[DataRequired()])
+    email = StringField("Email*", validators=[DataRequired(), Email(message="Enter a valid Email"), Length(max=120)])
+    message = TextAreaField("Message*", validators=[DataRequired()])
     submit = SubmitField("Submit")
